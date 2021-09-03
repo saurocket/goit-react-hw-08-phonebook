0546d045db150afe5c5b0043bdcb39e7ad71bb4f
Yauheni Luzakou <luzakou@yandex.ru>
2021-09-03 14:11:03 +0200

    fix-auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 
 import {Header} from "./Components/Header/Header";
 import {useDispatch, useSelector} from "react-redux";
-import {getAuth, getRememberMe} from "./redux/authReducer/authSelectors/authSelectors";
+import {getAuth, getErrors, getRememberMe} from "./redux/authReducer/authSelectors/authSelectors";
 import {Route, Switch, useHistory} from 'react-router-dom'
 import {PresentationPage} from "./Components/PresentationPage/PresentationPage";
 import Footer from "./Components/Footer/Footer";
@@ -16,6 +16,7 @@ import {actions} from "./redux/authReducer/authActions";
 
 
 export const App = ()  => {
+    const error = useSelector(getErrors)
     const auth = useSelector(getAuth)
     const rememberMe = useSelector(getRememberMe)
     const dispatch = useDispatch()
@@ -35,10 +36,17 @@ export const App = ()  => {
                 const authObject = await JSON.parse(getAuthFromLS)
                 dispatch(actions.authUser(authObject))
                 history.push(`${authObject.user.name}/contacts`)
+                return
+            }
+            if(error?.includes('authenticate')){
+                await localStorage.removeItem('auth')
+                 await dispatch(actions.authUser(null))
+                history.push('/')
             }
         }
+
         createAuthObject()
-    },[auth])
+    },[auth, error])
 
   return (
       <>
